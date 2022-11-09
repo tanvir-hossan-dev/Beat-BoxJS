bd2af027e3f5a3279949385ba70974f464d484cf
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-11-09 23:43:11 +0600

    50% complete

---
+++ utils.js
@@ -0,0 +1,38 @@
+/**
+ * Beat class that keeps track of playing the audio
+ * HINT: Make sure to pass in the audioSrc as parameter to create a new audio track
+ * HINT: Create a play function to play the audio if called
+ */
+class Beat {
+  //   constructor(audioSrc) {
+  //     this.audio = new audio(audioSrc);
+  //     console.log(this.audio);
+  //     this.audio.play();
+  //   }
+  //   play = () => {
+  //     this.audio.currentTime = 0;
+  //     this.audio.play();
+  //   };
+}
+
+/**
+ * Button class that keeps track of the button color based on a press
+ */
+class Button {
+  constructor(color, keyCode) {}
+
+  /**
+   * Set the button color based on color specified
+   */
+  setButtonColorInHTML = () => {};
+
+  /**
+   * Select function to set the background color and boxShadow
+   */
+  select = () => {};
+
+  /**
+   * Deselect function to reset background color and boxShadow
+   */
+  deselect = () => {};
+}
